5f139655d2824bd349e69309153f919cb14abe68
Ananya Subramaniam <ananyasub955@gmail.com>
2025-06-26 16:39:57 -0700

    session variables created and destroyed when user logs out

--- src/User/components/UserNavbar.jsx
+++ src/User/components/UserNavbar.jsx
@@ -2,17 +2,45 @@ import React from 'react'
 import logo from '../../assets/logo.png';
 import { NavLink, useNavigate } from 'react-router-dom';
 
+const url = window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://itws-4500-s25-team6.eastus.cloudapp.azure.com/node";
+
 const UserNavbar = () => {
+    const navigate = useNavigate();
+
+    const logoutUser = async () => {
+        try {
+            const response = await fetch(`${url}/logout`, {
+                credentials: "include",
+            });
+            if (response.ok) {
+                // setUserId(false);
+                // setIsLoggedIn(false);
+                navigate("/login");
+            } else {
+                console.error("Logout failed");
+            }
+        } catch (error) {
+            console.error("Error during logout:", error);
+        }
+    };
+
     return (
         <nav className="navbar navbar-expand-sm px-4" id="navbar">
-            <a className="navbar-brand" href="/">
+            <a className="navbar-brand" href="/user/dashboard">
                 <img src={logo} alt="Codoc Academy Logo" style={{ width: '200px' }} />
             </a>
             <div className="container-fluid">
 
                 <ul className="navbar-nav ms-auto fs-5">
                     <li className="nav-item">
-                        <NavLink className="nav-link bg-darkFuschia text-white rounded px-4" to="/logout">Logout</NavLink>
+                        <button
+                            className="nav-link bg-darkFuschia text-white rounded px-4"
+                            onClick={() => logoutUser()}
+                            >
+                            Logout
+                        </button>
                     </li>
                 </ul>
             </div>
